0442b5039fec8e8c6c58d3eb76fa60efd620e03c
akash <akash619@protonmail.com>
2019-06-13 01:18:22 +0530

    Added Login Form for Doctor & Routing in React

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Bulma.scss';
+import Header from './Header'
+// import DocLogin from './Doclogin'
+// import { Route, Link, BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return(
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App;
