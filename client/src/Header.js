09fc4e361eb07b55e4582b9ba3ae66cbd2bfa109
Naman Modi <modi.naman14@gmail.com>
2019-06-13 13:10:44 +0530

    Fixed Routing More Than One Child Element Issue in Header Component

--- client/src/Header.js
+++ client/src/Header.js
@@ -7,69 +7,73 @@ class Header extends Component {
   render() {
     return (
       <div>
-    <div classNameName="App">
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="/">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-          </a>
-          <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a href='/' className="navbar-item">
-              Home
-      </a>
-            <a className="navbar-item">
-              Documentation
-      </a>
-      <a href='/doctor/register' className="navbar-item">
-              Register
-      </a>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">
-                More
-        </a>
-              <div className="navbar-dropdown">
+        <div classNameName="App">
+          <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="/">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+              </a>
+              <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+            <div id="navbarBasicExample" className="navbar-menu">
+              <div className="navbar-start">
+                <a href='/' className="navbar-item">
+                  Home
+                </a>
                 <a className="navbar-item">
-                  About
-          </a>
-                <a className="navbar-item">
-                  Jobs
-          </a>
-                <a className="navbar-item">
-                  Contact
-          </a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">
-                  Report an issue
-          </a>
+                  Documentation
+                </a>
+                <a href='/doctor/register' className="navbar-item">
+                  Register
+                </a>
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">
+                    More
+                  </a>
+                  <div className="navbar-dropdown">
+                    <a className="navbar-item">
+                      About
+                    </a>
+                    <a className="navbar-item">
+                      Jobs
+                    </a>
+                    <a className="navbar-item">
+                      Contact
+                      </a>
+                    <hr className="navbar-divider" />
+                    <a className="navbar-item">
+                      Report an issue
+                     </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons">
+                    <a href='/doctor/register' className="button is-light">
+                      Register
+                    </a>
+                    <a href='/doctor/login' className="button is-success">
+                      Log in
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a href='/doctor/login' className="button is-success">
-                  Log in
-            </a>
-              </div>
-            </div>
-          </div>
+          </nav>
         </div>
-      </nav>
-
-    </div>
-    <Router>
-      <Route path='/doctor/login' component={Doclogin} />
-      {/* <Route path='/doctor/register' component={DocRegister} /> */}
-    </Router>
-    </div>
+        <Router>
+          <div>
+            <Route path='/doctor/login' component={Doclogin} />
+            <Route path='/doctor/register' component={DocRegister} />
+          </div>
+        </Router>
+      </div>
     );
   }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router()
+
+
+
 router.get('/', (err, res)=>{
   res.render('index');
 })
+
+
+
 module.exports =router;
