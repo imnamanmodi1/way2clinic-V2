1a1ee60cab8b5fcee5f58e1ae249ca38160c04f9
akash <akash619@protonmail.com>
2019-06-15 00:46:43 +0530

    added doctor display card

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Bulma.scss';
 import Header from './Header'
+import DocOnBoarding from './DocOnBoarding';
+import Footer from './Footer'
+import DoctorProfile from './DoctorProfile'
 // import DocLogin from './Doclogin'
 // import { Route, Link, BrowserRouter } from 'react-router-dom';
 
@@ -8,7 +11,8 @@ function App() {
   return (
     <>
       <Header />,
-      <DocProfile />
+      {/* <DocOnBoarding /> */}
+      <DoctorProfile />
       <Footer />
     </>
   )
